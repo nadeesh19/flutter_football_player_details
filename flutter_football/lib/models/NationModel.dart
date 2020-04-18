0f411beb9e971d3f2f7fce02d1b73d71aa1282d6
Nadeesha Maduman <>
2020-04-18 17:02:09 +0530

    flags Added to Project

--- flutter_football/lib/models/NationModel.dart
+++ flutter_football/lib/models/NationModel.dart
@@ -11,10 +11,10 @@ class NationModel {
 
 List<NationModel> nations = [
   NationModel(nationName: "Argentina", imagePath: "images/flags/argentina.png", countryId: "52"),
-  NationModel(nationName: "Argentina", imagePath: "images/flags/argentina.png", countryId: "54"),
-  NationModel(nationName: "Argentina", imagePath: "images/flags/argentina.png", countryId: "21"),
-  NationModel(nationName: "Argentina", imagePath: "images/flags/argentina.png", countryId: "14"),
-  NationModel(nationName: "Argentina", imagePath: "images/flags/argentina.png", countryId: "18"),
-  NationModel(nationName: "Argentina", imagePath: "images/flags/argentina.png", countryId: "27"),
-  NationModel(nationName: "Argentina", imagePath: "images/flags/argentina.png", countryId: "45"),
+  NationModel(nationName: "Brazil", imagePath: "images/flags/brazil.png", countryId: "54"),
+  NationModel(nationName: "Germany", imagePath: "images/flags/germany.png", countryId: "21"),
+  NationModel(nationName: "England", imagePath: "images/flags/england.png", countryId: "14"),
+  NationModel(nationName: "France", imagePath: "images/flags/france.png", countryId: "18"),
+  NationModel(nationName: "Italy", imagePath: "images/flags/italy.png", countryId: "27"),
+  NationModel(nationName: "Spain", imagePath: "images/flags/spain.png", countryId: "45"),
 ];
